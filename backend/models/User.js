e4d552af8b6bab766efa9211aabf4ee2c4f64c82
bassmaelabassi <elabassibasma31@gmail.com>
2025-06-18 11:57:55 +0200

    ajout schema pour user

---
+++ backend/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  nom: { type: String, required: true },
+  prenom: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  telephone: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['conducteur', 'expediteur', 'admin'], default: 'expediteur' },
+  isVerified: { type: Boolean, default: false },
+  badgeVerifie: { type: Boolean, default: false }
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('User', userSchema);
