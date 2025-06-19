463ed9ac44efb081773133a73d8c6903ee88f30e
bassmaelabassi <elabassibasma31@gmail.com>
2025-06-19 23:24:04 +0200

    ajout les routes demandes

---
+++ backend/routes/demandeRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const {
+  createDemande,
+  getDemandesForConducteur,
+  updateDemandeStatus,
+  getHistoriqueDemandes,
+  deleteDemande,
+} = require('../controllers/demandeControllers');
+
+const protect = require('../middlewares/authMiddleware');
+const roleMiddleware = require('../middlewares/roleMiddleware');
+
+router.post('/', protect, roleMiddleware(['expediteur']), createDemande);
+router.get('/conducteur', protect, roleMiddleware(['conducteur']), getDemandesForConducteur);
+router.put('/:id', protect, roleMiddleware(['conducteur']), updateDemandeStatus);
+router.get('/historique', protect, roleMiddleware(['expediteur']), getHistoriqueDemandes);
+router.delete('/:id', protect, roleMiddleware(['expediteur']), deleteDemande);
+
+module.exports = router;
